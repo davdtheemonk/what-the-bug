2db9585f8b20ef75636d7b655c716c82a2b5df6e
ProbablyX <83953506+davdtheemonk@users.noreply.github.com>
2022-05-05 18:36:58 +0300

    Update index.js

--- src/Home/index.js
+++ src/Home/index.js
@@ -207,6 +207,7 @@ React.useEffect(()=>{
   
     return(
         <div >
+      <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="mugalladavG" data-description="Support me on Buy me a coffee!" data-message="Buy me a coffee :)" data-color="#5F7FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
           
               <BootstrapDialog
         onClose={handleClose}
