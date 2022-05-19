eafff4e665038341a0133d6619a18fcd7377c54c
ProbablyX <83953506+davdtheemonk@users.noreply.github.com>
2022-05-19 19:40:58 +0300

    Update index.js

--- src/Home/index.js
+++ src/Home/index.js
@@ -206,7 +206,7 @@ React.useEffect(()=>{
     };
   
     return(
-        <div >
+        <div className="home_cont_over">
       <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="mugalladavG" data-description="Support me on Buy me a coffee!" data-message="Buy me a coffee :)" data-color="#5F7FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
           
               <BootstrapDialog
