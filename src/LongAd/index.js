1fe7b96673f09710db44b9069e2c563125bac2ea
davdtheemonk <parishniclaus@gmail.com>
2022-06-21 17:33:26 +0300

    ads

--- src/LongAd/index.js
+++ src/LongAd/index.js
@@ -1,20 +1,19 @@
 import React from "react"
 import {Adsense} from '@ctrl/react-adsense';
-
+import  "./styles.css"
 
 
 
 export default function LongAd(){
 
     return(
-       
+       <div className="ad-bar">
       <Adsense
   client="ca-pub-1377163247342129"
   slot="8487722237"
-  style={{ width: 728, height: 90 }}
-  format=""
-/>
 
+/>
+</div>
 
     )
 }
